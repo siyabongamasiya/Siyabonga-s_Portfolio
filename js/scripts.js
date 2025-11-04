96dbf4065cf787abd3bcd7c96d05ece323ee0163
siyabonga <siyabongakhanyile76@gmail.com>
2025-11-04 09:01:39 +0200

    mod:added the optional call in the script to try to prevent the null error in the browser

--- js/scripts.js
+++ js/scripts.js
@@ -53,7 +53,7 @@ const sendEMail = async() => {
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contactForm");
   const submitButton = document.getElementById("submitButton");
-  // const fields = form.querySelectorAll("input, textarea");
+  const fields = form.querySelectorAll("input, textarea");
   const responseText = document.getElementById("responseText")
 
   const checkValidity = () => {
@@ -61,20 +61,20 @@ document.addEventListener("DOMContentLoaded", () => {
     submitButton.disabled = !allValid;
     responseText.classList.add("d-none");
     // Show red borders / feedback as user types
-    // fields.forEach(field => {
-    //   if (!field.checkValidity()) {
-    //     field.classList.add("is-invalid");
-    //     field.classList.remove("is-valid");
-    //   } else {
-    //     field.classList.remove("is-invalid");
-    //     field.classList.add("is-valid");
-    //   }
-    // });
+    fields?.forEach(field => {
+      if (!field.checkValidity()) {
+        field.classList.add("is-invalid");
+        field.classList.remove("is-valid");
+      } else {
+        field.classList.remove("is-invalid");
+        field.classList.add("is-valid");
+      }
+    });
   };
 
-  // fields.forEach(field => field.addEventListener("input", checkValidity));
+  fields?.forEach(field => field.addEventListener("input", checkValidity));
   
-  form.addEventListener("submit", async (e) => {
+  form?.addEventListener("submit", async (e) => {
   e.preventDefault(); 
 
   const formData = new FormData(form);
